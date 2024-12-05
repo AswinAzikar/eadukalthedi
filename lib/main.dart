f19d76cdaffa99e21fca83c3304a241d2bff5f1e
cbabi2023 <abijithcbbsccs2021@gmail.com>
2024-12-05 10:34:38 +0530

    ok

--- lib/main.dart
+++ lib/main.dart
@@ -16,6 +16,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+
       //TODO: implement theme change
       theme: AppTheme.lightTheme,
       debugShowCheckedModeBanner: false,
