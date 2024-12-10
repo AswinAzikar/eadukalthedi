7b9791eea0602c199e701f5146de04e4289d4aca
cbabi2023 <abijithcbbsccs2021@gmail.com>
2024-12-10 06:13:42 +0530

    home page bottom section white space

--- lib/view/home_screen/home_screen.dart
+++ lib/view/home_screen/home_screen.dart
@@ -5,40 +5,24 @@ import 'package:eadukalthedi/view/home_screen/home_screen_controller.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    
     final providerObj = Provider.of<HomeScreenController>(context);
 
     return Scaffold(
       body: DefaultTabController(
-        length: 5, 
+        length: 5,
         child: Padding(
           padding: const EdgeInsets.only(
               left: paddingLarge, right: paddingLarge, top: paddingLarge),
           child: Column(
             children: [
-              
-              
-
-              SizedBox(
-                  height: .02 *
-                      SizeUtils
-                          .height), 
-
-              
+              SizedBox(height: .02 * SizeUtils.height),
               searchAndSort(),
-
-              SizedBox(
-                  height: .02 *
-                      SizeUtils
-                          .height), 
-
-              
+              SizedBox(height: .02 * SizeUtils.height),
               TabBar(
                 overlayColor: WidgetStateProperty.resolveWith<Color?>(
                   (states) {
@@ -52,14 +36,11 @@ class HomeScreen extends StatelessWidget {
                     const BorderRadius.all(Radius.circular(paddingXL)),
                 tabAlignment: TabAlignment.start,
                 isScrollable: true,
-                labelColor: Colors.black, 
-                unselectedLabelColor:
-                    Colors.grey, 
-                indicatorColor: greenTemp, 
+                labelColor: Colors.black,
+                unselectedLabelColor: Colors.grey,
+                indicatorColor: greenTemp,
                 tabs: providerObj.getCategoryTabs(),
               ),
-
-              
               Expanded(
                 child: TabBarView(
                   children: List.generate(
@@ -68,6 +49,12 @@ class HomeScreen extends StatelessWidget {
                   ),
                 ),
               ),
+              Column(
+                children: List.generate(
+                  5,
+                  (index) => gapLarge,
+                ),
+              )
             ],
           ),
         ),
@@ -75,16 +62,15 @@ class HomeScreen extends StatelessWidget {
     );
   }
 
-  
   Row searchAndSort() {
     return Row(
       children: [
         Expanded(
           child: TextFormField(
             decoration: InputDecoration(
-              fillColor: pureWhite, 
-              filled: true, 
-              hintText: "Search here", 
+              fillColor: pureWhite,
+              filled: true,
+              hintText: "Search here",
               suffixIcon: Icon(
                 Icons.search,
                 size: 0.07 * SizeUtils.width,
@@ -97,7 +83,7 @@ class HomeScreen extends StatelessWidget {
             ),
           ),
         ),
-        gap, 
+        gap,
         Container(
           decoration: BoxDecoration(
             borderRadius: BorderRadius.circular(padding),
@@ -112,13 +98,12 @@ class HomeScreen extends StatelessWidget {
     );
   }
 
-  
   Row appBarSection() {
     return const Row(
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
       children: [
-        Icon(Icons.menu), 
-        Icon(Icons.notifications_active), 
+        Icon(Icons.menu),
+        Icon(Icons.notifications_active),
       ],
     );
   }
