2771a2fb10c33b07c4f74001d905feaf8e5861b2
Aswinazikar <azwinaswin8@gmail.com>
2024-12-17 23:49:31 +0530

    "added some profiel screens"

---
+++ lib/view/profile_screen/profile_major.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class ProfileMajor extends StatefulWidget {
+  const ProfileMajor({super.key});
+
+  @override
+  State<ProfileMajor> createState() => _ProfileMajorState();
+}
+
+class _ProfileMajorState extends State<ProfileMajor> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+
+
+
+      
+    );
+  }
+}
